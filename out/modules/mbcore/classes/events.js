6821e130b22ce46b4d30976c002ffe2d0b3bcd52
ShiCheng Lu <shichenglu0708@gmail.com>
2021-12-06 22:55:30 -0500

    undo my fuckup

--- out/modules/mbcore/classes/events.js
+++
@@ -1,3 +0,0 @@
-import EventEmitter from "./eventemitter.js";
-const Events = new EventEmitter();
-export default Events;
--- out/src/index.js
+++
@@ -1,6 +0,0 @@
-import { world } from "mojang-minecraft";
-import { MBCPlayer } from "../modules/mbcore.js";
-world.events.beforeChat.subscribe((evd) => {
-    let plr = MBCPlayer.get(evd.sender.nameTag);
-    plr.executeCommand(`title @s actionbar $[{evd.sender.nameTag}]\n${evd.message}`);
-});
