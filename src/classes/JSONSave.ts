247305e4ea5f107b3d642ea563da7e957461e9ba
MajestikButter <yonnicaminas@gmail.com>
2022-07-29 03:58:31 -0400

    added async option to JSONSave

--- src/classes/JSONSave.ts
+++ src/classes/JSONSave.ts
@@ -13,6 +13,10 @@ export class JSONSave {
 
   private static json = "{}";
 
+  static get dataLength() {
+    return this.json.length;
+  }
+
   static getJSON() {
     return JSON.parse(this.json);
   }
@@ -31,31 +35,57 @@ export class JSONSave {
       if (ident.type !== ScoreboardIdentityType.fakePlayer) continue;
       str += ident.displayName;
     }
-    str = str.replace(/<\$>\\/g, "");
+    str = str.replace(/ÿ/g, '"').replace(/þ/g, "\\");
     str = str ? JSON.parse(`"${str}"`) : "{}";
     const parsed = JSON.parse(str);
     this.json = str;
     return parsed;
   }
-  static save() {
-    for (let ident of JSONSaveSB.objective.getParticipants()) {
-      if (ident.type !== ScoreboardIdentityType.fakePlayer) continue;
-      JSONSaveSB.reset(`"${ident.displayName}"`);
-    }
 
+  static save(async: boolean, minSaves?: number): void | Promise<void>;
+  static save(async?: false): void;
+  static async save(async: true, minSaves: number): Promise<void>;
+  static save(async = false, minSaves?: number) {
     let str = JSON.stringify(this.json).slice(1, -1);
     const chunks = str.length / this.maxStoreLength;
-    for (let i = 0; i < chunks; i++) {
+
+    const save = (i: number) => {
       const data = str.slice(0, this.maxStoreLength);
 
       let target = '"';
-      if (data.startsWith('"')) target += "<$>\\";
-      if (data.endsWith("\\")) target += `${data.slice(0, -1)}<$>\\\\`;
+      if (data.startsWith('"') && data.endsWith("\\"))
+        target += `ÿ${data.slice(1, -1)}þ`;
+      else if (data.endsWith("\\")) target += `${data.slice(0, -1)}þ`;
+      else if (data.startsWith('"')) target += `ÿ${data.slice(1)}`;
       else target += data;
       target += '"';
       const success = JSONSaveSB.set(target, i);
       if (!success) console.log(target);
       str = str.slice(this.maxStoreLength);
+    };
+
+    if (async) {
+      return new Promise<void>(async (resolve, reject) => {
+        for (let ident of JSONSaveSB.objective.getScores()) {
+          if (ident.participant.type !== ScoreboardIdentityType.fakePlayer)
+            continue;
+          JSONSaveSB.reset(`"${ident.participant.displayName}"`);
+          if (ident.score % (minSaves ?? 200) == 0) await null;
+        }
+
+        for (let i = 0; i < chunks; i++) {
+          save(i);
+          if (i % (minSaves ?? 200) == 0) await null;
+        }
+        resolve();
+      });
+    } else {
+      for (let ident of JSONSaveSB.objective.getParticipants()) {
+        if (ident.type !== ScoreboardIdentityType.fakePlayer) continue;
+        JSONSaveSB.reset(`"${ident.displayName}"`);
+      }
+      for (let i = 0; i < chunks; i++) save(i);
     }
+    return;
   }
 }
